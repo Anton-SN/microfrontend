54a881d1402d46c7d73335a3796570ee94facace
Anton Sivagin <ansn.onward@gmail.com>
2023-03-30 19:33:25 +0300

    init: add initial container package

---
+++ monorepo/container/src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+console.log(123)
+
+root.render(<h1>Hello world</h1>);
---
+++ monorepo/container/webpack/webpack.dev.js
@@ -0,0 +1,42 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = devConfig = {
+  mode: 'development',
+  output: {
+    publicPath: 'http://localhost:3000/',
+  },
+  devServer: {
+    port: 3000,
+    historyApiFallback: {
+      index: 'index.html',
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-react', {"runtime": "automatic"}], '@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  resolve: {
+    extensions: ['.ts', '.js']
+  }
+}
