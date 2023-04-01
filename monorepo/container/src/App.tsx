4635c0f5e98dc8de24e92236b4b4db8c7a8eacaf
Anton Sivagin <ansn.onward@gmail.com>
2023-04-01 08:39:49 +0300

    fix: final fix for forkflow

--- monorepo/container/src/App.tsx
+++ monorepo/container/src/App.tsx
@@ -3,7 +3,6 @@ import React, { lazy } from 'react';
 const AuthLazy = lazy(() => import('./Apps/AuthApp'));
 
 const App = () => {
-    console.log(123)
     return (
         <>
             <h1>Hello world!</h1>
