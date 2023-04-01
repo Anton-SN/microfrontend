d8f228bece8282608c9b6b99eddffad2221f9a05
Anton Sivagin <ansn.onward@gmail.com>
2023-04-01 08:31:47 +0300

    test-commit

--- monorepo/container/src/App.tsx
+++ monorepo/container/src/App.tsx
@@ -3,6 +3,7 @@ import React, { lazy } from 'react';
 const AuthLazy = lazy(() => import('./Apps/AuthApp'));
 
 const App = () => {
+    console.log(123)
     return (
         <>
             <h1>Hello world!</h1>
