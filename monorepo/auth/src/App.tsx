c81412ceba16f8c8d8e98800cf3ad5d41fbf86bf
Anton Sivagin <ansn.onward@gmail.com>
2023-04-06 22:24:33 +0300

    feat: add warning to console

--- monorepo/auth/src/App.tsx
+++ monorepo/auth/src/App.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 
-const App: React.FC = () => <h1>Auth project</h1>
+const App: React.FC = () => {
+    console.warn('start auth')
+    return <h1>Auth project</h1>
+}
 
 export default App;
