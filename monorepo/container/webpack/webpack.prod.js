b8942f4320ee163051d609985a3d8c2169239058
Anton Sivagin <ansn.onward@gmail.com>
2023-04-06 22:12:01 +0300

    fix: correct publicPath in webpack

--- monorepo/container/webpack/webpack.prod.js
+++ monorepo/container/webpack/webpack.prod.js
@@ -9,7 +9,7 @@ const prodConfig = {
   mode: 'production',
   output: {
     filename: '[name].[contenthash].js',
-    publicPath: '/auth/latest/',
+    publicPath: '/container/latest/',
   },
   plugins: [
     new ModuleFederationPlugin({
