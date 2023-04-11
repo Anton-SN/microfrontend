57dbccd671a2d5c34948c646c03b46ca890c6a0d
Anton Sivagin <ansn.onward@gmail.com>
2023-04-11 17:19:34 +0300

    feat: add routes to container

--- monorepo/container/src/App.tsx
+++ monorepo/container/src/App.tsx
@@ -1,14 +1,20 @@
 import React, { lazy } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const AuthLazy = lazy(() => import('./Apps/AuthApp'));
 
 const App = () => {
     console.warn('start container')
     return (
-        <>
-            <h1>Hello world!</h1>
-            <AuthLazy />
-        </>
+    <>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<h1>Hello world!</h1>} />
+                <Route path="/auth" element={<AuthLazy />}/>
+                <Route path="*" element={<h1>404 not found</h1>} />
+            </Routes>
+        </BrowserRouter>
+    </>
     )
 };
 
