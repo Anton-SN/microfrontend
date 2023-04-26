b4ae5848ae2ea8c23771693bc5d507a8f61f2d29
Anton Sivagin <ansn.onward@gmail.com>
2023-04-26 21:33:43 +0300

    feat: create basic form

--- monorepo/auth/src/ui/Input/index.tsx
+++ monorepo/auth/src/ui/Input/index.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 
 interface Props {
+    name: string,
     onChange: (event: React.SyntheticEvent) => void,
+    title: string,
+    type?: React.HTMLInputTypeAttribute,
 }
-export const Input: React.FC<Props> = ({ onChange }) => {
-    return <input type="text" onChange={onChange} />
+export const Input: React.FC<Props> = ({ name, title, onChange, type = 'text',  }) => {
+    return (
+        <div>
+            <label htmlFor={name} className="block text-sm font-medium leading-6 text-gray-900">{title}</label>
+            <input name={name} type={type} onChange={onChange} className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+        </div>
+    )
 }
--- monorepo/auth/src/widgets/LoginForm/index.tsx
+++ monorepo/auth/src/widgets/LoginForm/index.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import { Input } from '../../ui';
 
 export const LoginForm: React.FC = () => {
-    return <>
-        <Input onChange={(e) => { console.log(e)}} />
-    </>
+    return <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-6" action="#" method="POST">
+            <Input name="email" title="Email address" type="email" onChange={() => { console.log(123)}} />
+            <Input name="password" title="Password" type="password" onChange={() => { console.log(123)}} />
+            <div>
+                <button type="submit"
+                        className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign
+                    in
+                </button>
+            </div>
+        </form>
+        <p className="mt-10 text-center text-sm text-gray-500">
+            Not a member?
+            <Link to="/auth/signup" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"><span> Sign Up</span></Link>
+        </p>
+    </div>
 }
