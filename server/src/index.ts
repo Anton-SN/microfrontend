96cc6fe0194f992c0f348209439f01e035cdce82
Anton Sivagin <ansn.onward@gmail.com>
2023-04-29 20:13:57 +0300

    feat: connet with mongoDB, mongoose and create post controller

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,29 @@
 import fastify from 'fastify'
+import mongoose from 'mongoose';
+import { UserModel } from './models'
 
 const server = fastify()
 
-server.get('/ping', async (request, reply) => {
+const url = "mongodb+srv://lemondev:z3DYiEHhqwffTeGS@users.iilcmal.mongodb.net/?retryWrites=true&w=majority";
+
+server.register(require('@fastify/mongodb'), { forceClose: true, url })
+mongoose.connect(url)
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.log('Error connecting to MongoDB:', err));
+
+server.get('/ping', async () => {
   return 'pong\n'
+})
+
+server.post('/users', async function( req, reply) {
+  try {
+    const user = new UserModel(req.body)
+    await user.save();
+    reply.code(201).send({ message: 'User created successfully' });
+  } catch (error) {
+    console.error(error);
+    reply.code(500).send({ message: 'Error creating user' });
+  }
 })
 
 server.listen({ port: 8080 }, (err, address) => {
@@ -11,6 +31,5 @@ server.listen({ port: 8080 }, (err, address) => {
     console.error(err)
     process.exit(1)
   }
-  console.info(address)
   console.info(`Server listening at ${address}`)
 })
---
+++ server/src/models/user.ts
@@ -0,0 +1,17 @@
+import { Schema, model } from 'mongoose';
+
+const schema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = model('User', schema)
+
+export { User }
