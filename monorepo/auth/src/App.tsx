d468f31a9e03138e924e9f1796e24d758f31735e
Anton Sivagin <ansn.onward@gmail.com>
2023-04-29 12:32:36 +0300

    test commit

--- monorepo/auth/src/App.tsx
+++ monorepo/auth/src/App.tsx
@@ -23,6 +23,7 @@ const App: React.FC<AppProps> = ({ onNavigate }) => {
                 };
                 window.addEventListener('[auth] navigated', navigationEventHandler);
                 console.log(123);
+                console.log(123);
                 onNavigate(location.pathname)
 
                 return () => {
